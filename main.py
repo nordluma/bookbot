980be3e45db7638bafea2463da48a7d85e0f3fd1
nordluma <maxnordlund.95@hotmail.com>
2024-10-30 20:05:00 +0200

    feat: count words in text

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 def main():
     with open("books/frankenstein.txt") as f:
         text = f.read()
-        print(text)
+
+    print(count_words(text))
+
+
+def count_words(text: str) -> int:
+    return len(text.split())
+
+
 
 
 if __name__ == "__main__":
